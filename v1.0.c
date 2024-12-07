e30b855e941ad512480e24c87e5e4fbf9028d866
Frfiradilica <150937319+Frfiradilica@users.noreply.github.com>
2024-12-07 18:55:11 +0100

    Create v1.0.c
    Osnovna verzija sa porukom pozdrava

---
+++ v1.0.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Dobrodošli u jednostavan konvertor valuta!\n");
+    printf("Ovaj program će omogućiti osnovne funkcionalnosti konverzije.\n");
+    printf("Više opcija će biti dodato u narednim verzijama.\n");
+    return 0;
+}
