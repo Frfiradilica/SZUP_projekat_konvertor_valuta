4bb0455bc54b8f9ff1cb9f3befa96b1741a4d005
Frfiradilica <150937319+Frfiradilica@users.noreply.github.com>
2024-12-14 18:54:44 +0100

    Poslednja dopuna u projekat.c
    Dodavanje jos 3 valute

--- projekat.c
+++ projekat.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 
-void ucitajKurs(double kurs[3][3], const char* nazivFajla) {
+#define BROJ_VALUTA 6
+
+void ucitajKurs(double kurs[BROJ_VALUTA][BROJ_VALUTA], const char* nazivFajla) {
     FILE* fajl = fopen(nazivFajla, "r");
     if (fajl == NULL) {
         printf("Greška pri otvaranju datoteke!\n");
         return;
     }
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 0; i < BROJ_VALUTA; i++) {
+        for (int j = 0; j < BROJ_VALUTA; j++) {
             fscanf(fajl, "%lf", &kurs[i][j]);
         }
     }
     fclose(fajl);
 }
 
-double konvertuj(double iznos, int izValute, int uValutu, double kurs[3][3]) {
+double konvertuj(double iznos, int izValute, int uValutu, double kurs[BROJ_VALUTA][BROJ_VALUTA]) {
     return iznos * kurs[izValute - 1][uValutu - 1];
 }
 
 int main() {
     printf("Dobrodošli u konvertor valuta!\n");
 
-    double kurs[3][3];
+    double kurs[BROJ_VALUTA][BROJ_VALUTA];
     ucitajKurs(kurs, "kursna_lista.txt");
 
     int izValute, uValutu;
@@ -30,11 +32,16 @@ int main() {
     printf("Unesite iznos: ");
     scanf("%lf", &iznos);
 
-    printf("Unesite valutu iz koje konvertujete (1-EUR, 2-RSD, 3-USD): ");
+    printf("Unesite valutu iz koje konvertujete (1-EUR, 2-RSD, 3-USD, 4-GBP, 5-CHF, 6-JPY): ");
     scanf("%d", &izValute);
 
-    printf("Unesite valutu u koju konvertujete (1-EUR, 2-RSD, 3-USD): ");
+    printf("Unesite valutu u koju konvertujete (1-EUR, 2-RSD, 3-USD, 4-GBP, 5-CHF, 6-JPY): ");
     scanf("%d", &uValutu);
+
+    if (izValute < 1 || izValute > BROJ_VALUTA || uValutu < 1 || uValutu > BROJ_VALUTA) {
+        printf("Greška: neispravan izbor valute!\n");
+        return 1;
+    }
 
     double rezultat = konvertuj(iznos, izValute, uValutu, kurs);
     printf("Rezultat: %.2f\n", rezultat);
