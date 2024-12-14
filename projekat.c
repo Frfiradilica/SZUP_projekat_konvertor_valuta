b420f0a41fb06af50132353e2d5ee6e9bffdbdbb
Frfiradilica <150937319+Frfiradilica@users.noreply.github.com>
2024-12-14 17:29:00 +0100

    Dodavanje kursa u projekat.c
    Dodata osnovna kursna lista za konverziju

--- projekat.c
+++ projekat.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("Dobrodošli u jednostavan konvertor valuta!\n");
+    printf("Dobrodošli u konvertor valuta!\n");
+
+    double kurs[3][3] = {
+        {1.0, 117.5, 0.85},
+        {0.0085, 1.0, 0.0072},
+        {1.18, 138.0, 1.0}
+    };
+
+    printf("Kursna lista dodata za osnovne valute (EUR, RSD, USD).\n");
     return 0;
 }
